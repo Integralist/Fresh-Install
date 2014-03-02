b9e682428bba1a3d03a09ea592b13b284733576a
Integralist <mark.mcdx@gmail.com>
2014-03-02 16:25:26 +0000

    Add vim plugin install script

--- provision.sh
+++ provision.sh
@@ -72,6 +72,22 @@ function symlink_dotfiles() {
   done
 }
 
+function install_vim_plugins() {
+  cd "$HOME/.vim/bundle"
+
+  plugins=( kien/ctrlp.vim jlangston/tomorrow-night-vim tpope/vim-markdown tpope/vim-cucumber \
+            scrooloose/syntastic ervandew/supertab tpope/vim-repeat tpope/vim-commentary \
+            mileszs/ack.vim tpope/vim-endwise bling/vim-airline edkolev/tmuxline.vim \
+            mattn/webapi-vim vim-scripts/Gist.vim mattn/emmet-vim airblade/vim-gitgutter \
+            scrooloose/nerdtree tpope/vim-haml tpope/vim-surround othree/html5.vim \
+            godlygeek/tabular vim-scripts/camelcasemotion )
+
+  for item in "${plugins[@]}"
+  do
+    git clone "https://github.com/$item.git"
+  done
+}
+
 install_xcode()
 install_homebrew()
 install_brews()
