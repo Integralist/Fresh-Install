18b5e2fc519bf4fb9a504f56dd497bd212612620
Integralist <mark.mcdx@gmail.com>
2014-03-02 16:25:51 +0000

    Delete bundle content and add script that allows quick install of plugins on new machine

---
+++ vim-plugins.sh
@@ -0,0 +1,14 @@
+cd "$HOME/.vim/bundle"
+
+plugins=( kien/ctrlp.vim jlangston/tomorrow-night-vim tpope/vim-markdown tpope/vim-cucumber \
+          scrooloose/syntastic ervandew/supertab tpope/vim-repeat tpope/vim-commentary \
+          mileszs/ack.vim tpope/vim-endwise bling/vim-airline edkolev/tmuxline.vim \
+          mattn/webapi-vim vim-scripts/Gist.vim mattn/emmet-vim airblade/vim-gitgutter \
+          scrooloose/nerdtree tpope/vim-haml tpope/vim-surround othree/html5.vim \
+          godlygeek/tabular vim-scripts/camelcasemotion tpope/vim-fireplace tpope/vim-classpath \
+          guns/vim-clojure-static )
+
+for item in "${plugins[@]}"
+do
+  git clone "https://github.com/$item.git"
+done
