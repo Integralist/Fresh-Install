e298fea783aa81cfe6b06d383f6fc07d8739f05b
Jake Champion <jakechampion.jake2@googlemail.com>
2015-09-11 21:16:03 +0100

    update brew install script location

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@ function install_xcode() {
 }
 
 function install_homebrew() {
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
 function install_brews() {
